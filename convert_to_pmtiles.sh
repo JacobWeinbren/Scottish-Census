df5da60b1ef33735e78e625fc5c832835dd6c1ed
Jacob Weinbren <jacobweinbren@gmail.com>
2024-05-28 19:05:05 +0100

    Compressed scottish census map

--- convert_to_pmtiles.sh
+++ convert_to_pmtiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directories containing GeoJSON files
-directories=("output/output-areas")
+directories=("output/output-areas" "output/intersected")
 
 # Loop through each directory
 for dir in "${directories[@]}"; do
@@ -13,23 +13,29 @@ for dir in "${directories[@]}"; do
         output_file="output/pmtiles/${file##*/}"
         output_file="${output_file%.geojson}.pmtiles"
         
-        # Run Tippecanoe to convert GeoJSON to PMTiles
-         tippecanoe --output="$output_file" \
-               --layer="maplayer" \
-               --no-feature-limit \
-               --no-tile-size-limit \
-               --detect-shared-borders \
-               --coalesce-fraction-as-needed \
-               --coalesce-densest-as-needed \
-               --coalesce-smallest-as-needed \
-               --increase-gamma-as-needed \
-               --coalesce \
-               --reorder \
-               --minimum-zoom=0 \
-               --maximum-zoom=17 \
-               --force \
-               --simplification=20 \
-               "$file"
+        # Set common Tippecanoe options
+        common_options=(
+            --output="$output_file"
+            --layer="maplayer"
+            --detect-shared-borders
+            --coalesce-fraction-as-needed
+            --coalesce-densest-as-needed
+            --coalesce-smallest-as-needed
+            --increase-gamma-as-needed
+            --coalesce
+            --reorder
+            --minimum-zoom=0
+            --maximum-zoom=17
+            --force
+            --simplification=20
+        )
+        
+        # Add directory-specific options
+        if [[ "$dir" == "output/output-areas" ]]; then
+            tippecanoe "${common_options[@]}" --no-feature-limit --no-tile-size-limit "$file"
+        elif [[ "$dir" == "output/intersected" ]]; then
+            tippecanoe "${common_options[@]}" --no-feature-limit "$file"
+        fi
         
         echo "Finished conversion of $file at $(date)"
     done
